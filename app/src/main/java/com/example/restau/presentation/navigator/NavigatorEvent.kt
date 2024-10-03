85628c474d16bf68dcc69d95ae9bf073443270b6
itsmemanuu <m.pachecom2@uniandes.edu.co>
2024-10-03 11:30:36 -0500

    Analytics Modifications for Nearby Restaurants Based on User's Preferred Cuisine

--- app/src/main/java/com/example/restau/presentation/navigator/NavigatorEvent.kt
+++ app/src/main/java/com/example/restau/presentation/navigator/NavigatorEvent.kt
@@ -1,9 +1,10 @@
 package com.example.restau.presentation.navigator
 
+import android.content.Context
+
 sealed class NavigatorEvent {
 
     class SelectedChange(val selected: Int) : NavigatorEvent()
-
-    class AuthCheck() : NavigatorEvent()
+    class OnStart(val context: Context) : NavigatorEvent()
 
 }
