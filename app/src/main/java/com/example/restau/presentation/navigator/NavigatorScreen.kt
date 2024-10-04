ed5100669a2661b7e69f215f213cbe4e2f677a16
Manuela Pacheco Malagón <98624209+itsmemanuu@users.noreply.github.com>
2024-10-03 21:44:39 -0500

    Feature/cuisine based restaurant analytics #91 (#93)
    * Analytics Modifications for Nearby Restaurants Based on User's Preferred Cuisine
    
    * Analytics Modifications for Nearby Restaurants Based on User's Preferred Cuisine

--- app/src/main/java/com/example/restau/presentation/navigator/NavigatorScreen.kt
+++ app/src/main/java/com/example/restau/presentation/navigator/NavigatorScreen.kt
@@ -1,18 +1,28 @@
 package com.example.restau.presentation.navigator
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -22,6 +32,7 @@ import com.example.restau.presentation.navigation.Route
 import com.example.restau.presentation.navigation.itemsMap
 import com.example.restau.presentation.navigator.components.NavBar
 import com.example.restau.presentation.splashscreen.SplashScreen
+import com.example.restau.ui.theme.Poppins
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
@@ -35,6 +46,11 @@ fun NavigatorScreen(
 
     val currentUser by navigatorViewModel.currentUser.collectAsState()
 
+    val context = LocalContext.current
+
+    val alertMessage by navigatorViewModel.alertMessage.collectAsState()
+
+
     LaunchedEffect(currentEntry) {
         navigatorViewModel.onEvent(
             NavigatorEvent.SelectedChange(itemsMap[(currentEntry?.destination?.route)?: Route.HomeScreen.route]?: 0)
@@ -43,9 +59,11 @@ fun NavigatorScreen(
 
     LaunchedEffect(Unit) {
         navigatorViewModel.onEvent(
-            NavigatorEvent.AuthCheck()
+            NavigatorEvent.OnStart(context)
         )
     }
+
+
 
     if (!navigatorViewModel.showSplash) {
         systemUiController.setSystemBarsColor(
@@ -71,6 +89,60 @@ fun NavigatorScreen(
                 }
             }
         ) {
+            if (alertMessage != null) {
+                AlertDialog(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .background(Color.White, shape = RoundedCornerShape(10.dp)),
+                    containerColor = Color.White,
+                    onDismissRequest = {
+                        navigatorViewModel.alertMessage.value = null
+                    },
+                    text = {
+                        Text(
+                            text = alertMessage!!,
+                            fontFamily = Poppins,
+                            color = Color.DarkGray,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                    },
+                    confirmButton = {
+                        TextButton(
+                            onClick = {
+                                navigatorViewModel.alertMessage.value = null
+                                navController.navigate(Route.MapScreen.route)
+                            }
+                        ) {
+                            Text(
+                                text = "Let's go!",
+                                fontFamily = Poppins,
+                                color = MaterialTheme.colorScheme.secondary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 14.sp
+                            )
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(
+                            onClick = {
+                                navigatorViewModel.alertMessage.value = null
+                            }
+                        ) {
+                            Text(
+                                text = "Dismiss",
+                                fontFamily = Poppins,
+                                color = Color.Gray,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 14.sp
+                            )
+                        }
+                    }
+
+                )
+            }
+
+
             NavigatorContent(
                 modifier = Modifier.padding(
                     start = it.calculateStartPadding(
--- app/src/main/java/com/example/restau/presentation/navigator/NavigatorViewModel.kt
+++ app/src/main/java/com/example/restau/presentation/navigator/NavigatorViewModel.kt
@@ -1,22 +1,32 @@
 package com.example.restau.presentation.navigator
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.restau.domain.usecases.AuthUseCases
+import com.example.restau.domain.usecases.LocationUseCases
 import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
+import java.net.URL
 import javax.inject.Inject
 
 @HiltViewModel
 class NavigatorViewModel @Inject constructor(
-    private val authUseCases: AuthUseCases
+    private val authUseCases: AuthUseCases,
+    private val locationUseCases: LocationUseCases
 ) : ViewModel() {
 
     var selected by mutableIntStateOf(0)
@@ -27,15 +37,19 @@ class NavigatorViewModel @Inject constructor(
 
     var currentUser = MutableStateFlow<FirebaseUser?>(null)
 
+    val alertMessage = MutableStateFlow<String?>(null)
+
     fun onEvent(event: NavigatorEvent) {
         when (event) {
             is NavigatorEvent.SelectedChange -> {
                 selected = event.selected
             }
 
-            is NavigatorEvent.AuthCheck -> {
+            is NavigatorEvent.OnStart -> {
                 viewModelScope.launch {
                     authCheck()
+                    nearRestaurants(event.context)
+                    Log.d("NavigatorViewModel", "OnStart: ${currentUser.value}")
                 }
             }
         }
@@ -49,6 +63,64 @@ class NavigatorViewModel @Inject constructor(
         currentUser.value = getCurrentUser()
         Log.d("NavigatorViewModel", "authCheck: ${currentUser.value}")
         showSplash = false
+    }
+
+
+    private fun nearRestaurants(context: Context) {
+        if (hasLocationPermission(context)) {
+            viewModelScope.launch(Dispatchers.IO) {
+                try{
+                    val id = currentUser.first { it != null }?.uid
+                    val location = locationUseCases.getLocation.invoke().first()
+                    val lat = location?.latitude.toString()
+                    val lon = location?.longitude.toString()
+
+                    val apiURL = "http://34.134.5.98:8000/nearbyxcuisine/"
+
+                    val url = URL("$apiURL?&userID=$id&lat=$lat&lon=$lon")
+                    val connection = url.openConnection() as HttpURLConnection
+
+                    connection.requestMethod = "GET"
+
+                    val responseCode = connection.responseCode
+
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        val inputStream = connection.inputStream
+                        var response = inputStream.bufferedReader().use { it.readText() }.toString()
+                        response = response.replace("\"", "")
+                        if (response != "null") {
+
+                            alertMessage.value =
+                                if (response.firstOrNull()?.lowercaseChar() in listOf(
+                                        'a',
+                                        'e',
+                                        'i',
+                                        'o',
+                                        'u'
+                                    )
+                                ) "There's an $response restaurant near you" else "There's a $response restaurant near you"
+                        }
+                    }
+                }
+                catch (e: Exception) {
+                    Log.e("HomeViewModel", "Error during close restaurants", e)
+                }
+            }
+        }
+    }
+
+    private fun hasLocationPermission(context: Context): Boolean {
+        val fineLocationPermission = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val coarseLocationPermission = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        return fineLocationPermission == PackageManager.PERMISSION_GRANTED ||
+                coarseLocationPermission == PackageManager.PERMISSION_GRANTED
     }
 }
 
