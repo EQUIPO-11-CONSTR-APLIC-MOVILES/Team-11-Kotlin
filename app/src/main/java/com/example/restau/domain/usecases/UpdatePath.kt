825191127a63775999b61321ad940b8a35db923b
Manuela Pacheco Malagón <98624209+itsmemanuu@users.noreply.github.com>
2024-10-16 16:21:51 -0500

    Minor fixes on user session views #118 (#125)
    * Done modifications for Navigation Paths Analytics (#104)
    
    * Done fixes nav paths fixes
    
    * Done fixes nav paths fixes (#116)
    
    * Minor fixes on user session views done

--- app/src/main/java/com/example/restau/domain/usecases/UpdatePath.kt
+++ app/src/main/java/com/example/restau/domain/usecases/UpdatePath.kt
@@ -1,9 +1,10 @@
-package com.example.restau.domain.usecases.pathUseCases
+package com.example.restau.domain.usecases
 
 import com.example.restau.domain.repository.NavPathsRepository
 
 class UpdatePath (private val navPathsRepository: NavPathsRepository) {
-    suspend operator fun invoke(screensIDs: List<Int>, pathID: String): Boolean{
+    suspend operator fun invoke(screensIDs: List<Int>, pathID: String?): Boolean{
+        if (pathID == null) return false
         return navPathsRepository.updatePath(screensIDs, pathID)
     }
 }
