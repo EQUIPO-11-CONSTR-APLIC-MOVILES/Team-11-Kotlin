da2ac631341c1422abcc297645bd78cadfd89002
Daniel Escalante Perez <d.escalante@uniandes.edu.co>
2024-10-25 22:32:46 -0500

    Simplification of the LikedViewModel connectivity solution. Implementation of eventual connectivity in the ReviewListScreen, ReviewListViewModel and ReviewListState.

--- app/src/main/java/com/example/restau/presentation/reviewlist/ReviewListState.kt
+++ app/src/main/java/com/example/restau/presentation/reviewlist/ReviewListState.kt
@@ -4,5 +4,6 @@ import com.example.restau.domain.model.Review
 
 data class ReviewListState(
     val reviews: List<Review> = emptyList(),
-    val isLoading: Boolean = true
+    val isLoading: Boolean = true,
+    val showFallback: Boolean = false
 )
